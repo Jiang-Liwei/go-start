6756d70ee3bd798da2d629722f91df5fa29c2f85
Jiang-Liwei <8083015@qq.com>
2022-07-14 18:06:57 +0800

    if

--- 005_for/for.go
+++ 005_for/for.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	//最基础的方式，单个循环条件。
+	// 最基础的方式，单个循环条件。
 
 	i := 1
 	for i <= 3 {
@@ -15,13 +15,13 @@ func main() {
 	for j := 7; j <= 9; j++ {
 		fmt.Println(j)
 	}
-	//不带条件的 for 循环将一直重复执行， 直到在循环体内使用了 break 或者 return 跳出循环。
+	// 不带条件的 for 循环将一直重复执行， 直到在循环体内使用了 break 或者 return 跳出循环。
 
 	for {
 		fmt.Println("loop")
 		break
 	}
-	//你也可以使用 continue 直接进入下一次循环。
+	// 你也可以使用 continue 直接进入下一次循环。
 
 	for n := 0; n <= 5; n++ {
 		if n%2 == 0 {
